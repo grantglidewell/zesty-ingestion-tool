1c9052be37880048cf8ceac7b0fca8a102f61dc3
Grant Glidewell <grant.glidewell@zesty.io>
2018-08-20 13:07:03 -0700

    test scraping initial page

--- src/scraper.js
+++ src/scraper.js
@@ -2,41 +2,55 @@ const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 const chalk = require('chalk')
 const fs = require('fs')
+const path = require('path')
 
-module.exports = function(url, path) {
+module.exports = function(url, userPath) {
+  const dir = path.resolve(process.cwd(), userPath)
   // flow control
-  console.log('starting scrape of' + url)
-  scrape(url)
+  scrape(url, dir)
     .then(data => {
       // save the resources
-      console.log(chalk.red('scrape cheerio return'), data)
-      const resources = saveResource(parseResources(page, path), path)
+      // console.log(chalk.red('scrape cheerio return '), data)
+      const list = Array.from(data('a'))
+      // const resources = saveResource(parseResources(list, path), path)
       // determine if there are more links
-      return findInternalLinks(page)
+      // return findInternalLinks(page)s
     })
     .then()
     .catch(console.error)
 }
 
-const scrape = async url => {
-  const page = await fetch(url).then(data => data.text)
-
-  return cheerio.load(page, {
+const scrape = async (url, dir) => {
+  const page = await fetch(url).then(data => {
+    return data.text()
+  })
+  saveResource(page, dir)
+  const $ = cheerio.load(page, {
     withDomLvl1: true,
     normalizeWhitespace: false,
     xmlMode: true,
     decodeEntities: true
   })
+  return $
 }
 
-const parseResources = list => {
+const parseResources = (list, dir) => {
+  // console.log('Args in resources', list, dir)
   // returns a promise, containg an object with
   // 'dir/filename.css' as a key, and the data as the value
 }
 
-const saveResource = (resourceObject, path) => {
+const saveResource = (page, dir) => {
+  console.log(chalk.green('args in saveResource'), page, dir)
   // create non-existant dirs
+  if (fs.existsSync(dir)) {
+    fs.writeFileSync(`${dir}/index.html`, page)
+  } else {
+    fs.mkdirSync(dir)
+    fs.writeFileSync(`${dir}/index.html`, page)
+  }
   // write resource
+  // return something that says the file has been written and scraping can continue
 }
 
 const findInternalLinks = links => {
