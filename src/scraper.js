fdb904723d5fa8c1c9cb8a3c03adca7ee9c1ba10
Grant Glidewell <grant.glidewell@zesty.io>
2018-08-20 16:25:08 -0700

    scraping recursively through tree, not pretty, no statics

--- src/scraper.js
+++ src/scraper.js
@@ -12,22 +12,28 @@ module.exports = function(url, userPath) {
   // flow control
   // do some validation on the url
   // make sure it has a trailing slash
-  scrape(url, dir)
+  scrape({ url, dir })
     .then(data => {
       // identify resources that are not html (not in links)
       const list = Array.from(data('a'))
       const links = parseInternalLinks(list, url)
-      return sequenceUrlFetch(links, url).then(data => saveResource(data, dir))
+      return sequenceUrlFetch(links, url, dir)
     })
     .then(console.log)
     .catch(console.error)
 }
 
-const scrape = async url => {
-  console.log('scraping ' + chalk.yellow(url))
-  const page = await fetch(url).then(data => {
+const scrape = async opts => {
+  if (opts.domain) {
+    opts.FullURL = `${opts.domain}${opts.url}`
+  } else {
+    opts.FullURL = opts.url
+  }
+  console.log('scraping ' + chalk.yellow(opts.url))
+  const page = await fetch(opts.FullURL).then(data => {
     return data.text()
   })
+  saveHTML(page, opts.domain ? `${opts.dir}/${opts.url}` : opts.dir)
   const $ = cheerio.load(page, {
     withDomLvl1: true,
     normalizeWhitespace: false,
@@ -37,7 +43,7 @@ const scrape = async url => {
   return $
 }
 
-const parseInternalLinks = (list, url) => {
+const parseInternalLinks = list => {
   // filter out any outside links
   const links = list
     .filter(item => item.attribs.href && !item.attribs.href.includes('http'))
@@ -49,20 +55,20 @@ const parseInternalLinks = (list, url) => {
       )
       return acc
     }, [])
-    // get rid of duplicate entries
+  // get rid of duplicate entries
   const setLinks = new Set(links)
   return Array.from(setLinks)
   // scrape each subsequent
   // 'dir/filename.css' as a key, and the data as the value
 }
 
-const saveResource = (page, dir) => {
+const saveHTML = (page, dir) => {
   // console.log(chalk.green('args in saveResource'), page, dir)
   // maybe run this in a try catch?
   // create non-existant dirs
   if (fs.existsSync(dir)) {
     console.log('dir exists')
-    fs.writeFileSync(`${dir}/index${Math.floor(Math.random() * 10)}.html`, page)
+    fs.writeFileSync(`${dir}/index.html`, page)
   } else {
     fs.mkdirSync(dir)
     fs.writeFileSync(`${dir}/index.html`, page)
@@ -71,11 +77,13 @@ const saveResource = (page, dir) => {
   // write resource
 }
 
-const sequenceUrlFetch = (urls, domain) => {
-  const funcs = urls.map(url => () => scrape(domain + url))
-  return funcs.reduce(
-    (promise, func) =>
-      promise.then(result => func().then(Array.prototype.concat.bind(result))),
-    Promise.resolve([])
-  )
+const sequenceUrlFetch = (urls, domain, dir) => {
+  urls.map(url => {
+    return scrape({ domain, url, dir })
+  })
+  // return funcs.reduce(
+  //   (promise, func) =>
+  //     promise.then(result => func().then(Array.prototype.concat.bind(result))),
+  //   Promise.resolve([])
+  // )
 }
